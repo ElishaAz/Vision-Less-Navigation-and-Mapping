14da34c1ea0ea0d44ead6356be0ce93efa8310fa
Elisha Azaria <elishadar@gmail.com>
2024-12-02 14:10:33 +0200

    Merge close nodes

--- Assets/Scripts/Mapping/Inconsistencies.cs
+++ Assets/Scripts/Mapping/Inconsistencies.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Drone;
 using UnityEngine;
 
@@ -9,6 +10,17 @@ namespace Mapping
         [SerializeField] private DroneSensors sensors;
         [SerializeField] private float threshold = 1f;
         [SerializeField] private GameObject prefab;
+        [SerializeField] private int maxBackMerge = 5;
+        [SerializeField] private float mergeDistance = 1f;
+
+        private struct Inconsistency
+        {
+            public Vector3 Position;
+            public int Count;
+            public GameObject Prefab;
+        }
+
+        private readonly List<Inconsistency> inconsistencies = new List<Inconsistency>();
 
         private float lastRight;
         private float lastLeft;
@@ -21,13 +33,42 @@ namespace Mapping
 
         private void FixedUpdate()
         {
-            float right = sensors.right.DistanceNormalized;
-            float left = sensors.left.DistanceNormalized;
+            var right = sensors.right.DistanceNormalized;
+            var left = sensors.left.DistanceNormalized;
+            var position = sensors.DronePosition;
 
             if (Mathf.Abs(right - lastRight) > threshold || Mathf.Abs(left - lastLeft) > threshold)
             {
-                Instantiate(prefab, sensors.DronePosition + DroneView.DroneView.Offset, Quaternion.identity, transform);
+                bool merged = false;
+                for (int i = inconsistencies.Count - 1; i >= Mathf.Max(inconsistencies.Count - maxBackMerge, 0); i--)
+                {
+                    var lastIncon = inconsistencies[i];
+                    if (Vector3.Distance(lastIncon.Position, position) < mergeDistance)
+                    {
+                        // TODO: add time check
+                        // Weighted average
+                        lastIncon.Position = (lastIncon.Position * lastIncon.Count + position) / (lastIncon.Count + 1);
+                        lastIncon.Count++;
+                        lastIncon.Prefab.transform.position = lastIncon.Position + DroneView.DroneView.Offset;
+                        merged = true;
+                        Debug.Log($"Merged {lastIncon.Position}");
+                        break;
+                    }
+                }
+
+                if (!merged)
+                {
+                    var incon = new Inconsistency
+                    {
+                        Position = position,
+                        Count = 1,
+                        Prefab = Instantiate(prefab, sensors.DronePosition + DroneView.DroneView.Offset,
+                            Quaternion.identity, transform)
+                    };
+                    inconsistencies.Add(incon);
+                }
             }
+
             lastRight = right;
             lastLeft = left;
         }
