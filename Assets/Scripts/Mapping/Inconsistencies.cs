e2bfa9d3ed86623ee513b7dbbe7a8c53622e42e1
Elisha Azaria <elishadar@gmail.com>
2024-12-02 16:25:27 +0200

    Add edges

--- Assets/Scripts/Mapping/Inconsistencies.cs
+++ Assets/Scripts/Mapping/Inconsistencies.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Drone;
 using UnityEngine;
@@ -9,40 +8,88 @@ namespace Mapping
     {
         [SerializeField] private DroneSensors sensors;
         [SerializeField] private float threshold = 1f;
-        [SerializeField] private GameObject prefab;
+        [SerializeField] private GameObject nodePrefab;
+        [SerializeField] private GameObject edgePrefab;
         [SerializeField] private int maxBackMerge = 5;
         [SerializeField] private float mergeDistance = 1f;
+        [SerializeField] private float interval = 0.1f;
 
-        private struct Inconsistency
+        private readonly struct Sample
+        {
+            public readonly float Right;
+            public readonly float Left;
+            public readonly float Up;
+            public readonly float Down;
+            public readonly float Forward;
+            public readonly float Back;
+
+            public Sample(float right, float left, float up, float down, float forward, float back)
+            {
+                Right = right;
+                Left = left;
+                Up = up;
+                Down = down;
+                Forward = forward;
+                Back = back;
+            }
+        }
+
+        private class Node
         {
             public Vector3 Position;
             public int Count;
             public GameObject Prefab;
+            public Sample Before;
+            public Sample After;
         }
 
-        private readonly List<Inconsistency> inconsistencies = new List<Inconsistency>();
+        private class Edge
+        {
+            public Node From;
+            public Node To;
+            public GameObject Prefab;
 
-        private float lastRight;
-        private float lastLeft;
+            public void UpdatePosition()
+            {
+                Prefab.transform.position = (From.Position + To.Position) / 2 + DroneView.DroneView.Offset;
+                Prefab.transform.rotation = Quaternion.LookRotation(To.Position - From.Position);
+                Prefab.transform.localScale = new Vector3(1, 1, Vector3.Distance(From.Position, To.Position));
+            }
+        }
+
+        private readonly List<Node> nodes = new List<Node>();
+        private readonly List<Edge> edges = new List<Edge>();
+        private Node lastNode = null;
+
+        private Sample lastSample;
 
         private void Start()
         {
-            lastRight = sensors.right.DistanceNormalized;
-            lastLeft = sensors.left.DistanceNormalized;
+            lastSample = new Sample(sensors.right.DistanceNormalized, sensors.left.DistanceNormalized,
+                sensors.up.DistanceNormalized, sensors.down.DistanceNormalized,
+                sensors.front.DistanceNormalized, sensors.back.DistanceNormalized);
         }
+
+
+        private float nextUpdate;
 
         private void FixedUpdate()
         {
-            var right = sensors.right.DistanceNormalized;
-            var left = sensors.left.DistanceNormalized;
+            nextUpdate -= Time.fixedDeltaTime;
+            if (nextUpdate > 0) return;
+            nextUpdate = interval;
+            var sample = new Sample(sensors.right.DistanceNormalized, sensors.left.DistanceNormalized,
+                sensors.up.DistanceNormalized, sensors.down.DistanceNormalized,
+                sensors.front.DistanceNormalized, sensors.back.DistanceNormalized);
             var position = sensors.DronePosition;
 
-            if (Mathf.Abs(right - lastRight) > threshold || Mathf.Abs(left - lastLeft) > threshold)
+            if (Mathf.Abs(sample.Right - lastSample.Right) > threshold ||
+                Mathf.Abs(sample.Left - lastSample.Left) > threshold)
             {
                 bool merged = false;
-                for (int i = inconsistencies.Count - 1; i >= Mathf.Max(inconsistencies.Count - maxBackMerge, 0); i--)
+                for (int i = nodes.Count - 1; i >= Mathf.Max(nodes.Count - maxBackMerge, 0); i--)
                 {
-                    var lastIncon = inconsistencies[i];
+                    var lastIncon = nodes[i];
                     if (Vector3.Distance(lastIncon.Position, position) < mergeDistance)
                     {
                         // TODO: add time check
@@ -50,6 +97,21 @@ namespace Mapping
                         lastIncon.Position = (lastIncon.Position * lastIncon.Count + position) / (lastIncon.Count + 1);
                         lastIncon.Count++;
                         lastIncon.Prefab.transform.position = lastIncon.Position + DroneView.DroneView.Offset;
+
+                        foreach (var edge in edges)
+                        {
+                            if (edge.From == lastIncon || edge.To == lastIncon)
+                            {
+                                edge.UpdatePosition();
+                            }
+                        }
+
+                        if (lastNode != lastIncon)
+                        {
+                            // TODO add edge
+                        }
+
+                        lastNode = lastIncon;
                         merged = true;
                         Debug.Log($"Merged {lastIncon.Position}");
                         break;
@@ -58,19 +120,33 @@ namespace Mapping
 
                 if (!merged)
                 {
-                    var incon = new Inconsistency
+                    var node = new Node
                     {
                         Position = position,
                         Count = 1,
-                        Prefab = Instantiate(prefab, sensors.DronePosition + DroneView.DroneView.Offset,
+                        Before = lastSample,
+                        After = sample,
+                        Prefab = Instantiate(nodePrefab, sensors.DronePosition + DroneView.DroneView.Offset,
                             Quaternion.identity, transform)
                     };
-                    inconsistencies.Add(incon);
+                    if (lastNode != null)
+                    {
+                        var edge = new Edge
+                        {
+                            From = (Node)lastNode,
+                            To = node,
+                            Prefab = Instantiate(edgePrefab, transform)
+                        };
+                        edge.UpdatePosition();
+                        edges.Add(edge);
+                    }
+
+                    nodes.Add(node);
+                    lastNode = node;
                 }
             }
 
-            lastRight = right;
-            lastLeft = left;
+            lastSample = sample;
         }
     }
 }
