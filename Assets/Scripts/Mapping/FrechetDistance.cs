4f4b4c8ae27ee4b7a3daada06d6fd97d738fc40d
Elisha Azaria <elishadar@gmail.com>
2024-12-30 14:14:29 +0200

    Remove second implementation

--- Assets/Scripts/Mapping/FrechetDistance.cs
+++ Assets/Scripts/Mapping/FrechetDistance.cs
@@ -40,38 +40,5 @@ namespace Mapping
 
             return ca[left.Count - 1, right.Count - 1];
         }
-
-        public static float FD<T>(List<T> left, List<T> right, Distance<T> distance)
-        {
-            var ca = new float[left.Count, right.Count];
-            for (int i = 0; i < left.Count; i++)
-            {
-                for (int j = 0; j < right.Count; j++)
-                {
-                    ca[i, j] = -1;
-                }
-            }
-            return CalculateDistance(left, right, distance, ca, left.Count - 1, right.Count - 1);
-        }
-
-        private static float CalculateDistance<T>(List<T> left, List<T> right, Distance<T> distance,
-            float[,] ca, int i, int j)
-        {
-            if (ca[i, j] > -1)
-            {
-                return ca[i, j];
-            }
-
-            float dist = distance(left[i], right[j]);
-
-            float c_i = i > 0 ? CalculateDistance<T>(left, right, distance, ca, i - 1, j) : float.PositiveInfinity;
-            float c_j = j > 0 ? CalculateDistance<T>(left, right, distance, ca, i, j - 1) : float.PositiveInfinity;
-            float c_ij = (i > 0 && j > 0)
-                ? CalculateDistance<T>(left, right, distance, ca, i - 1, j - 1)
-                : float.PositiveInfinity;
-
-            ca[i, j] = Mathf.Max(Mathf.Min(c_i, c_j, c_ij), dist);
-            return ca[i, j];
-        }
     }
 }
