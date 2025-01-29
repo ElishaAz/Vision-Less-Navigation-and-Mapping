8dad05003be675501193a65fe68434663c597dcc
Elisha Azaria <elishadar@gmail.com>
2025-01-29 13:35:29 +0200

    Add lidar stick

--- Assets/Scripts/DroneView/DroneView.cs
+++ Assets/Scripts/DroneView/DroneView.cs
@@ -8,12 +8,13 @@ namespace DroneView
     public class DroneView : MonoBehaviour
     {
         public static Vector3 Offset { get; private set; }
-        
+
         [SerializeField] private Drone.Drone drone;
         [SerializeField] private DroneSensors sensors;
 
         [SerializeField] private Vector3 offset;
         [SerializeField] private GameObject[] lidarPrefs;
+        [SerializeField] private GameObject[] lidarSticks;
         [SerializeField] private Transform holder;
 
         [SerializeField] private float interval = 0.1f;
@@ -45,20 +46,38 @@ namespace DroneView
 
                 for (var i = 0; i < sensors.Lidars.Length; i++)
                 {
-                    if (lidarPrefs[i] == null) continue;
-                    
                     var lidar = sensors.Lidars[i];
-                    if (float.IsNaN(lidar.Distance)) continue;
-                    if (float.IsInfinity(lidar.Distance)) continue;
-                    var lidarRotation = lidar.transform.localRotation;
+                    if (float.IsNaN(lidar.Distance) || float.IsInfinity(lidar.Distance))
+                    {
+                        if (i < lidarSticks.Length)
+                            lidarSticks[i]?.SetActive(false);
+                        continue;
+                    }
 
+                    var lidarRotation = lidar.transform.localRotation;
                     var spawnPosition = dronePosition +
                                         (droneRotation *
                                          (lidarRotation * Vector3.forward * lidar.Distance +
                                           lidar.transform.localPosition));
-                    var lidarPos = Instantiate(lidarPrefs[i], spawnPosition, lidarRotation * droneRotation);
-                    lidarPos.transform.SetParent(holder);
+
+                    if (lidarPrefs[i] != null)
+                    {
+                        var lidarPos = Instantiate(lidarPrefs[i], spawnPosition, lidarRotation * droneRotation);
+                        lidarPos.transform.SetParent(holder);
+                    }
+
+                    if (i < lidarSticks.Length && lidarSticks[i] != null)
+                    {
+                        var stick = lidarSticks[i];
+                        stick.SetActive(true);
+
+                        var diff = spawnPosition - dronePosition;
+                        stick.transform.position = (dronePosition + spawnPosition) / 2;
+                        stick.transform.rotation = Quaternion.LookRotation(diff);
+                        stick.transform.localScale = new Vector3(1, 1, Vector3.Distance(dronePosition, spawnPosition));
+                    }
                 }
+
                 yield return new WaitForSeconds(interval);
             }
         }
