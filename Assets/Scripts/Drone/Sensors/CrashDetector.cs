846f9a58dfd4eaeef50017782feae307ce127450
Elisha Azaria <elishadar@gmail.com>
2025-09-29 11:35:47 +0300

    Allow disabling pausing on the first crash

--- Assets/Scripts/Drone/Sensors/CrashDetector.cs
+++ Assets/Scripts/Drone/Sensors/CrashDetector.cs
@@ -1,26 +1,33 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace Drone.Sensors
 {
-	public class CrashDetector : MonoBehaviour
-	{
-		public bool InCrash { get; private set; }
-		public int Crashes { get; private set; }
+    public class CrashDetector : MonoBehaviour
+    {
+        [SerializeField] private bool pauseOnCrash = true;
+        public bool InCrash { get; private set; }
+        public int Crashes { get; private set; }
 
-		private void OnCollisionEnter(Collision other)
-		{
-			if (Crashes == 0)
-			{
-				EditorApplication.isPaused = true;
-			}
-			InCrash = true;
-			Crashes++;
-		}
+        private void OnCollisionEnter(Collision other)
+        {
+            if (Crashes == 0 && pauseOnCrash)
+            {
+#if UNITY_EDITOR
+                EditorApplication.isPaused = true;
+#endif
+                pauseOnCrash = false;
+            }
 
-		private void OnCollisionExit(Collision other)
-		{
-			InCrash = false;
-		}
-	}
+            InCrash = true;
+            Crashes++;
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            InCrash = false;
+        }
+    }
 }
